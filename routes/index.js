67fc550245b0b2f983349e412c34e13bf57f15d5
unknown <ntkien.info@gmail.com>
2019-02-21 21:15:46 +0700

    part2

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req,res){
+  var data = JSON.stringify(req.body);
+  
+  console.log(data);
+  
+});
+
+module.exports = router;
