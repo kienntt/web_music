6b473ea399b8ec362b92e6dfe43d1af0d3901c63
Izuna <reddevil241097@gmail.com>
2019-02-22 22:14:47 +0700

    Cc

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,8 @@ router.post("/search", urlencodedParser, function(req, res) {
   } else {
     let include = req.body.main_keyword.split(";");
     let exclude = req.body.exclude_keyword.split(";");
-
+    include = [...new Set(include)];
+    exclude = [...new Set(exclude)];
     /* ket noi elasticsearch */
     var client = new elasticsearch.Client({
       host: "http://192.168.1.210:9200",
@@ -28,12 +29,17 @@ router.post("/search", urlencodedParser, function(req, res) {
     var query = '{"query": { "bool": { "should": { "bool": { "must": [], "must_not": [] } } } }}';
     query = JSON.parse(query);
     for (let i = 0; i < include.length; i++) {
-      query['query']['bool']['should']['bool']['must'].push({ "match_phrase_prefix": { "lyrics": include[i] } });
+      if(include[i].trim() !== "") {
+        query['query']['bool']['should']['bool']['must'].push({ "match_phrase_prefix": { "lyrics": include[i] } });
+      }
     }
     for (let i = 0; i < exclude.length; i++) {
-      query['query']['bool']['should']['bool']['must_not'].push({ "match_phrase_prefix": { "lyrics": exclude[i] } });
+      if(exclude[i].trim() !== "") {
+        query['query']['bool']['should']['bool']['must_not'].push({ "match_phrase_prefix": { "lyrics": exclude[i] } });
+      }
     }
-    
+    let show_include = include.filter(item => item.trim() !== "");
+    let show_exclude = exclude.filter(item => item.trim() !== "");
     client.search(
       {
         index: "songtest",
@@ -50,7 +56,7 @@ router.post("/search", urlencodedParser, function(req, res) {
           });
           res.cookie('main_keyword',include,{ expires: new Date(Date.now() + 900000), httpOnly: true })
           res.cookie('exclude_keyword',exclude,{ expires: new Date(Date.now() + 900000), httpOnly: true })
-          res.render("search/result", { data: data });
+          res.render("search/result", { data: data, show_include, show_exclude });
         }
       }
     );
--- routes/search.js
+++ routes/search.js
@@ -11,7 +11,8 @@ router.post('/byresult', function (req, res, next) {
     let exclude = req.body.exclude_keyword.split(";");
     include = include.concat(old_main_keyword);
     exclude = exclude.concat(old_exclude_keyword);
-    console.log(include);
+    include = [...new Set(include)];
+    exclude = [...new Set(exclude)];
     /* ket noi elasticsearch */
     var client = new elasticsearch.Client({
         host: "http://192.168.1.210:9200",
@@ -21,11 +22,19 @@ router.post('/byresult', function (req, res, next) {
     var query = '{"query": { "bool": { "should": { "bool": { "must": [], "must_not": [] } } } }}';
     query = JSON.parse(query);
     for (let i = 0; i < include.length; i++) {
-        query['query']['bool']['should']['bool']['must'].push({ "match_phrase_prefix": { "lyrics": include[i] } });
+        if(include[i].trim() !== "") {
+            query['query']['bool']['should']['bool']['must'].push({ "match_phrase_prefix": { "lyrics": include[i] } });
+        }
     }
     for (let i = 0; i < exclude.length; i++) {
-        query['query']['bool']['should']['bool']['must_not'].push({ "match_phrase": { "lyrics": exclude[i] } });
+        if(exclude[i].trim() !== "") {
+            query['query']['bool']['should']['bool']['must_not'].push({ "match_phrase": { "lyrics": exclude[i] } });
+        }
     }
+    let show_include = include.filter(item => item.trim() !== "");
+    let show_exclude = exclude.filter(item => item.trim() !== "");
+
+    show_include
 
     client.search(
         {
@@ -43,7 +52,7 @@ router.post('/byresult', function (req, res, next) {
                 });
                 res.cookie('main_keyword', include, { expires: new Date(Date.now() + 900000), httpOnly: true })
                 res.cookie('exclude_keyword', exclude, { expires: new Date(Date.now() + 900000), httpOnly: true })
-                res.render("search/result", { data: data });
+                res.render("search/result", { data: data , show_include, show_exclude});
             }
         }
     );
